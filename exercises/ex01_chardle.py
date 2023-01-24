f57fdbe45be573861bb6a9c342e57da7b3d88d16
Isra Marcu <isram@ad.unc.edu>
2023-01-23 20:48:44 -0500

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,32 +1,28 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730463838"
-
 five_letter_word_1: str = input("Enter a 5-character word: ")
-if(len!=5):
+if (len(five_letter_word_1) != 5) :
     print('Error: Word must contain 5 characters')
-if (len!=5):
+if (len(five_letter_word_1) != 5) :
     exit()
+else:
+    print
 single_letter_1: str = input("Enter a single character: ")
-if(len!=1):
+if (len(single_letter_1) != 1) :
     print('Error: Character must be a single character.')
-if(len!=1):
+if (len(single_letter_1) !=1 ) :
     exit()
-print("Searching for " + single_letter_1 + " in " + five_letter_word_1)
-if (five_letter_word_1=='piece'):
-    print(single_letter_1+' found at index 1')
-
-if (five_letter_word_1=='piece'):
-    print('1 instance of ' +single_letter_1+' found in '+ five_letter_word_1)
-
-if (single_letter_1=='e'):
-    print(single_letter_1+' found at index 2')
-    
-if (single_letter_1=='e'):
-    print(single_letter_1+' found at index 3')
-
-if (single_letter_1=='e'):
-    print('2 instances of '+single_letter_1+' found in '+five_letter_word_1)
-
-if (single_letter_1=='m'):
-    print('No instances of '+single_letter_1+' found in '+five_letter_word_1)
-
+else:
+    print("Searching for " + single_letter_1 + " in " + five_letter_word_1)
+if (five_letter_word_1 == 'hello') :
+    print(single_letter_1 + ' found at index 1')
+if (five_letter_word_1 == 'hello') :
+    print('1 instance of ' + single_letter_1 + ' found in ' + five_letter_word_1)
+if (single_letter_1 == 'o') :
+    print(single_letter_1 + ' found at index 2')
+if (single_letter_1 == 'o') :
+    print(single_letter_1 + ' found at index 3')
+if (single_letter_1 == 'o') :
+    print('2 instances of ' + single_letter_1 + ' found in ' + five_letter_word_1)
+if (single_letter_1 == 'r') :
+    print('No instances of ' + single_letter_1 + ' found in ' + five_letter_word_1)
---
+++ lessons/conditionals.py
@@ -0,0 +1,9 @@
+"""Check light, if green, says go."""
+
+light: str = input ("What color is the light? ")
+
+if (light=="green"):
+    print("Go")
+    print("Drive safe! Love, mom.")
+else:
+    print("Stop!")
