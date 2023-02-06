f884f06a86ebd6a4327942bb6018be134b178cd7
Isra Marcu <isram@ad.unc.edu>
2023-02-05 20:44:01 -0500

    excersize 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,28 +1,39 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730463838"
 five_letter_word_1: str = input("Enter a 5-character word: ")
-if (len(five_letter_word_1) != 5) :
+if (len(five_letter_word_1) != 5):
     print('Error: Word must contain 5 characters')
-if (len(five_letter_word_1) != 5) :
+    
     exit()
-else:
-    print
+
 single_letter_1: str = input("Enter a single character: ")
-if (len(single_letter_1) != 1) :
+if (len(single_letter_1) != 1):
     print('Error: Character must be a single character.')
-if (len(single_letter_1) !=1 ) :
+if (len(single_letter_1) != 1):
     exit()
 else:
-    print("Searching for " + single_letter_1 + " in " + five_letter_word_1)
-if (five_letter_word_1 == 'hello') :
+    print('Searching for ' + single_letter_1 + ' in ' + five_letter_word_1)
+
+count: int = 0
+
+if (five_letter_word_1[0] == single_letter_1):
+    print(single_letter_1 + ' found at index 0')
+    count = count + 1
+if (five_letter_word_1[1] == single_letter_1):
     print(single_letter_1 + ' found at index 1')
-if (five_letter_word_1 == 'hello') :
-    print('1 instance of ' + single_letter_1 + ' found in ' + five_letter_word_1)
-if (single_letter_1 == 'o') :
+    count = count + 1
+if (five_letter_word_1[2] == single_letter_1):
     print(single_letter_1 + ' found at index 2')
-if (single_letter_1 == 'o') :
+    count = count + 1
+if (five_letter_word_1[3] == single_letter_1):
     print(single_letter_1 + ' found at index 3')
-if (single_letter_1 == 'o') :
-    print('2 instances of ' + single_letter_1 + ' found in ' + five_letter_word_1)
-if (single_letter_1 == 'r') :
+    count = count + 1
+if (five_letter_word_1[4] == single_letter_1):
+    print(single_letter_1 + ' found at index 4')
+    count = count + 1
+if (count == 0):
     print('No instances of ' + single_letter_1 + ' found in ' + five_letter_word_1)
+if (count == 1):
+    print('1 instance of ' + single_letter_1 + ' found in ' + five_letter_word_1)
+if (count >= 2):
+    print(str(count) + ' instances of ' + single_letter_1 + ' found in ' + five_)
