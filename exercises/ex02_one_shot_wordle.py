447c0cd9b4c0fdcaa4924188bdd7508196fa73d3
Isra Marcu <isram@ad.unc.edu>
2023-02-14 21:46:16 -0500

    exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,6 +1,7 @@
 """EX02 - One-Shot Wordle - Loops!"""
 __author__ = "730463838"
 
+
 secret_word: str = "python"
 guess: str = input(f"What is your {len(secret_word)} guess? ")
 
@@ -17,22 +18,23 @@ while guess_game:
         while i < len(secret_word):
             if guess[i] == secret_word[i]:
                 squares = f"{squares}{GREEN_BOX}"
-            else: # printing yellow and white
+            else:  # printing yellow and white
                 match_idx = 0
                 found = False
                 while match_idx < len(secret_word):
-                    if guess[i] == secret_word[match_index]:
+                    if guess[i] == secret_word[match_idx]:
                         found = True
-                    match_index = match_idx + 1 
-                if found: squares = f"{squares}{YELLOW_BOX}"
-                else: squares = f"{squares}{WHITE_BOX}"
+                    match_idx = match_idx + 1 
+                if found: 
+                    squares = f"{squares}{YELLOW_BOX}"
+                else: 
+                    squares = f"{squares}{WHITE_BOX}"
             i = i + 1  
         print(squares)
         if guess == secret_word:
             print("Woo! You got it!")
         else:
-                print("Not quite. Play again soon!")
+            print("Not quite. Play again soon!")
         guess_game = False
-    else: # guess not the length of secret word
+    else:  # guess not the length of secret word
         guess = input(f"That was not {len(secret_word)} letters! Try again: ")
-
