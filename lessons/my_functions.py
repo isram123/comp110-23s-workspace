dd465c62dc8f3551f5d9b37e553d0c38d53b99d1
Isra Marcu <isram@ad.unc.edu>
2023-02-22 12:21:47 -0500

    my functions class

---
+++ lessons/my_functions.py
@@ -0,0 +1,11 @@
+"""things ill be importing"""
+
+def addition(x:int, y:int):
+    return x + y
+
+my_variable: str = "Hello"
+
+if __name__ == "__main__":
+    print("This should should only print when running main")
+else:
+    print("my_functions is being imported")
