a49f81e52ab12c5f20b62fdaba635ec98d932bcd
Isra Marcu <isram@ad.unc.edu>
2023-04-17 12:15:33 -0400

    river

--- exercises/ex09/bear.py
+++ exercises/ex09/bear.py
@@ -1,9 +1,22 @@
 """File to define Bear class"""
 
-class Bear:
-    
+class Bear:  
+
+    age: int
+    hunger_score: int
+
     def __init__(self):
+        self.age = 0
+        copy_bear: list = list[Bear]
+        if self.age > 5:
+            copy_bear.pop(Bear)
+        else:
+            copy_bear.append(Bear)
+        self.bears == copy_bear
+        hunger_score = 0 
         return None
     
     def one_day(self):
+        self.one_day += 1
         return None
+
--- exercises/ex09/fish.py
+++ exercises/ex09/fish.py
@@ -1,9 +1,21 @@
 """File to define Fish class"""
 
 class Fish:
+
+    age: int
     
     def __init__(self):
+        self.age = 0 
+        copy_fish: list = list[Fish]
+        for fish in self.fish:
+            if self.age > 3:
+                copy_fish.pop(fish)
+            else:
+                copy_fish.append(fish)
+        self.fish == copy_fish
+
         return None
     
     def one_day(self):
+        self.one_day += 1
         return None
